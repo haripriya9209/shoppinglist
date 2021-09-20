4800b29182aa319f5acf8e860bedf104d48561ea
haripriya9209 <s.haripriya9209@gmail.com>
2021-09-20 17:47:39 +0530

    delete the list

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+//declaring variables
 var addlist = document.getElementById("addlist");
 var add = document.getElementById("add");
+var li_item = document.getElementsByClassName("present");
 var ul = document.getElementById("list");
 var error = document.getElementById("error");
+var del = document.getElementsByClassName("btn-del");
 
+//adding items
 function addlistitem(){
     var li = document.createElement("li");
     li.append(document.createTextNode(addlist.value));
@@ -10,7 +14,7 @@ function addlistitem(){
     addlist.value = "";
 }
 
-
+//adding items on click
 add.addEventListener("click", function(){
     error.textContent ="";
     if(addlist.value.length>0)
@@ -23,6 +27,7 @@ add.addEventListener("click", function(){
     
 })
 
+//adding items on enter
 addlist.addEventListener("keypress", function(event){
     error.textContent ="";
     if(addlist.value.length>0 && event.code==="Enter")
@@ -30,3 +35,11 @@ addlist.addEventListener("keypress", function(event){
         addlistitem();
     }
 })
+
+//deleting item
+for(let i=0; i<del.length; i++){
+    del[i].addEventListener("click", function(){
+        console.log(i);
+        li_item[i].classList.add("add-item");
+    })
+}
